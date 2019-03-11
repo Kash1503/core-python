9f3c71eb8691fcad75f9bdd992acb40b4ad705e1
Kashif Javed <kash.animations@gmail.com>
2019-03-11 17:54:19 +0000

    Add test_not_in and test_between tests to byotest.py

--- byotest.py
+++ byotest.py
@@ -9,3 +9,12 @@ def test_not_equal(a, b):
 # Check to see if item is in collection
 def test_is_in(collection, item):
     assert item in collection, "{0} does not contain {1}".format(collection, item)
+    
+# check to see if item is not in the collection
+def test_not_in(collection, item):
+    assert item not in collection, "{0} contains {1}, which is unexpected".format(collection, item)
+    
+# test to see if value is between an upper and lower limit, inclusively
+def test_between(upper, number, lower):
+    assert number < upper, "{0} is larger than {1}, the upper limit".format(number, upper)
+    assert number > lower, "{0} is smaller than {1}, the lower limit".format(number, lower)
